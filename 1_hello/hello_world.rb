9af511f958edb708cb63621f01614b9e562ee0cf
excavator <str_token@mail.ru>
2019-11-26 21:50:24 +0300

    start

---
+++ 1_hello/hello_world.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+
+describe 'hello word' do
+	it 'returns true' do
+		expect('hello world').to eq('hello world')
+	end
+end	
+
---
+++ 2_address_validator/address_validator_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative 'address_validator'
+
+describe AddressValidator do
+
+  it 'returns false for incomlete address' do
+    address = { street: "123 Any Street", city: "Anytown"}
+    expect(
+      AddressValidator.valid?(address)
+    ).to eq(false)
+  end
+
+  it 'missing_parts returns an array of missing required parts' do
+    address = { street: "123 Any Street", city: "Anytown"}
+    expect(
+      AddressValidator.missing_parts(address)
+    ).to eq([:region, :postal_code, :country])
+  end
+
+end
